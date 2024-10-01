bac66bf9e7562c859b931fdb5a5f4053691672e9
Giammarco-Gaudini <g.gaudini@techfem.it>
2024-10-01 16:02:12 +0200

    Aggiunta grafica

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Ciao");
+document.getElementById('changeColorButton').addEventListener('click', function() {
+    // Genera un colore casuale
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+  });
+  
